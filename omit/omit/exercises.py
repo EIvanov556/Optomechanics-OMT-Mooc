071c58de274066b5dc6a91720147968ff686fb07
Edouard Ivanov <edouard.ivanov@lkb.upmc.fr>
2018-10-11 18:08:03 +0200

    Updated the way the GUI works, and finalise OpticsOnly

--- omit/omit/exercises.py
+++ omit/omit/exercises.py
@@ -3,62 +3,189 @@ from cmath import *
 from ipywidgets import interact, interactive, fixed, interact_manual
 # https://ipywidgets.readthedocs.io/en/stable/examples/Using%20Interact.html
 import matplotlib.pyplot as plt
-import ipywidgets as widgets
+from matplotlib.widgets import Slider, Button, RadioButtons, TextBox
+#import ipywidgets as widgets
 
 
 class InteractiveFunction:
-    params = dict()
-    main_fun = None
-    int_fun = None
+    """
+    Interactive GUI plotting Object.
+    Is given a set of widgets that are to be shown on the interactive UI.
+    To pass this onto the class, a dictionary called widgets must be created in the child, containing the information
+    of the widgets to the plotted. This is organised in the following way:
 
-    def run_raw(self):
-        if self.main_fun is None:
-            raise AttributeError('No function attributoed.')
-        else:
-            self.main_fun()
+    widgets = {
+        'widget_name': [UIWidget, {*args}]}
+    The UI widget is an object that must contain as attributes an init_fun, called at the initialisation, and a
+    reset_fun, used when the reset button is pressed.
+    The live updates of the widget are to be handled in the child class itself.
+    """
+    axcolor = 'lightgoldenrodyellow'
+    window_size = [50, 100, 1000, 800]
+    widgets = None
+    reset_pos = [0,0,0,0]
 
-    def run(self):
-        if self.int_fun is None:
-            raise AttributeError('No function attributoed.')
-        else:
-            self.int_fun()
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
+        plt.subplots_adjust(left=0.25, bottom=0.25)
+        self.fig_manager = plt.get_current_fig_manager()
+        self.fig_manager.window.setGeometry(self.window_size[0], self.window_size[1],
+                                            self.window_size[2], self.window_size[3])
 
+        for key in self.widgets.keys():
+            key_widget = self.widgets[key][0]
+            key_widget.init_fun()
+
+        self.resetax = plt.axes(self.reset_pos)
+        self.button = Button(self.resetax, 'Reset', color=self.axcolor, hovercolor='0.975')
+        self.button.on_clicked(self.reset_ui)
+
+    def reset_ui(self,event):
+        for key in self.widgets.keys():
+            key_widget = self.widgets[key][0]
+            reset_fun = key_widget.reset_fun
+            if reset_fun is not None:
+                reset_fun()
+
+class MatplotlibUIWidget:
+    """
+    A simple Object class to contain all the attributed required by the Interactive Function __init__.
+    """
+    def __init__(self, init_fun=None, update_fun=None, reset_fun=None):
+        self.init_fun = init_fun
+        self.update_fun = update_fun
+        self.reset_fun = reset_fun
 
 class OpticsOnly(InteractiveFunction):
-    params = dict([('eta', 1)])
+    """
+    An InteractiveFunction for light sent into a cavity, with no other effects given. The transmission is then plotted
+    as a function of the ratio between the loss coefficients (kext+k0)/kext
+    Everything is given in units of kext.
+    """
+    reset_pos = [0.85, 0.33, 0.1, 0.04]
 
-    def main_fun(self, eta=1):
-        # eta is k0/kext
-        # eta = params['eta']
-        omegas = np.arange(-5, 5, 0.01)  # in units of kext
-        susceptibility = 1 / (((1+eta)/2)+1j*omegas)
-        r = np.abs(1-susceptibility)**2
+    def __init__(self):
+        self.window_size = [50, 100, 1000, 800]
+        self.omega_range = 5  # axis will go from - omega_range to +omega_range
 
-        plt.plot(omegas, r)
-        plt.ylim(0, 1.05)
-        plt.xlabel('detuning from resonance')
-        plt.ylabel('R')
-        plt.show()
+        plot_xlength = 0.65
+        plot_ylength = plot_xlength
+        self.widgets = {
+            'plot': [MatplotlibUIWidget(self.plot_fig, self.update_plot_fig, None),
+                     {
+                         'plot_coords': [0.05,1-plot_ylength-0.02,plot_xlength,plot_ylength],
+                         'plot_range': [-self.omega_range, self.omega_range, 0, 1.1]
+                     }],
+            'axes': [MatplotlibUIWidget(self.init_axes, None, self.reset_axes),
+                     {
+                         'eta_pos': ['eta', [1, 0, 5, 0.1], [0.75, 1-plot_ylength-0.02+0.1-0.04, 0.2, 0.025]]
+                     }],
+            'explanatory_text': [MatplotlibUIWidget(self.show_text, self.update_text, None),
+                                 {
+                                     'text_pos': [0.01, 1 - 0.65 - 0.02 - 0.3, 1 - 0.02, 0.25]
+                                 }]}
+        self.params = {
+            'eta': [1, 0, 5, 0.1]}
+        super().__init__()
 
+    def main_fun(self, *pars):
+        """
+        Calculated the reflection coefficient of a Fabry-Perot cavity around its resonance frequency.
+        :param pars: Contains only one item: eta, the ratio k0+kext/kext
+        :return: the frequency range and the reflection coefficient in this range
+        """
+        omegas = np.arange(-self.omega_range, self.omega_range, 0.01)  # in units of kext
+        susceptibility = 1 / (((1 + pars[0]) / 2) + 1j * omegas)
+        r = np.abs(1 - susceptibility) ** 2
+        return omegas, r
+
+    def plot_fig(self):
+        default_vals = ()
+        for par in self.params.keys():
+            default_vals += (self.params[par][0],)
+        x, y = self.main_fun(*default_vals)
+        self.l, = plt.plot(x, y, lw=2, color='xkcd:black')
+        plot_coords = self.widgets['plot'][1]['plot_coords']
+        self.l.axes.set_position(plot_coords)
+        plot_range = self.widgets['plot'][1]['plot_range']
+        plt.axis(plot_range)
+
+    def update_plot_fig(self):
+        x, y = self.main_fun(*self.current_vals)
+        self.l.set_ydata(y)
+        self.fig.canvas.draw_idle()
+
+    def init_axes(self):
+        self.slider_axes = dict()
+        self.sliders = dict()
+        for i, par in enumerate(self.widgets['axes'][1].keys()):
+            slider_positions = self.widgets['axes'][1][par][2]
+            self.slider_axes[par] = plt.axes(slider_positions,
+                                             facecolor=self.axcolor)
+
+            slider_tag = self.widgets['axes'][1][par][0]
+            slider_pars = self.widgets['axes'][1][par][1]
+            self.sliders[par] = Slider(self.slider_axes[par], slider_tag,
+                                       slider_pars[1],
+                                       slider_pars[2],
+                                       valinit=slider_pars[0],
+                                       valstep=slider_pars[3])
+            self.sliders[par].on_changed(self.update_ui)
+
+    def reset_axes(self):
+        for ax in self.widgets['axes'][1].keys():
+            self.sliders[ax].reset()
+
+    def show_text(self):
+        default_vals = ()
+        for par in self.params.keys():
+            default_vals += (self.params[par][0],)
+        text = self.get_explanatory_text(*default_vals)
+
+        text_pos = self.widgets['explanatory_text'][1]['text_pos']
+        self.textax = plt.axes(text_pos, facecolor=self.axcolor)  # an axis needs to be created for each interactive object
+
+        text = self.get_explanatory_text(*default_vals)
+        self.textbox = TextBox(self.textax, '', initial=text, color=self.axcolor, hovercolor=self.axcolor,
+                               label_pad=0.01)
+
+    def update_text(self):
+        new_text = self.get_explanatory_text(*self.current_vals)
+        self.textbox.set_val(new_text)
+
+    @staticmethod
+    def get_explanatory_text(*pars):
+        """
+        Gives a short explanatory text about the current selected situation
+        :param pars:
+        :return: text to be displayed
+        """
+        eta = pars[0]
         if eta == 0:
-            print('Extremely overcoupled: \nThe losses are negligible compared to the rate at which light can escape. '
-                  '\nAll the light is then reflected back out of the cavity')
+            text = 'Extremely overcoupled: \nThe losses are negligible compared to the rate at which light can escape.\n' \
+                   'All the light is then reflected back out of the cavity'
         elif eta < 1:
-            print('Overcoupled: \nThe cavity losses are smaller than the external coupling rate.')
+            text = 'Overcoupled: \nThe cavity losses are smaller than the external coupling rate.'
         elif eta == 1:
-            print('Critical coupling:\nThe rate at which the light enters the cavity matches exactly the decay rate.'
-                  '\nAt resonance, all photons will be dissipated')
+            text = 'Critical coupling:\nThe rate at which the light enters the cavity matches exactly the decay rate.\n' \
+                   'At resonance, all photons will be dissipated'
         else:
-            bad_cavity_text = """Undercoupled:\nThis is the so-called "bad cavity" regime. All excitations die out 
-            faster than they can be extracted from the cavity.
-            """
-            print(bad_cavity_text)
+            text = 'Undercoupled:\nThis is the so-called "bad cavity" regime. All excitations die out ' \
+                   'faster than they can be extracted from the cavity.'
+        return text
 
-    def int_fun(self):
-        # NEEDS A PROPER GUI
-        eta_slider = widgets.FloatSlider(min=0, max=10, step=0.1, continuous_update=False, value=1)
-        interactive(self.main_fun, eta=eta_slider)
+    def update_ui(self, val):
+        for widget in self.widgets.keys():
+            update_fun = self.widgets[widget][0].update_fun
+            if update_fun is not None:
+                update_fun()
 
+    @property
+    def current_vals(self):
+        current_vals = ()
+        for par in self.widgets['axes'][1].keys():
+            current_vals += (self.sliders[par].val,)
+        return current_vals
 
 class OneToneExperiment(InteractiveFunction):
     params = dict([('eta', 1), ('g0', 1e2/1e5)])
